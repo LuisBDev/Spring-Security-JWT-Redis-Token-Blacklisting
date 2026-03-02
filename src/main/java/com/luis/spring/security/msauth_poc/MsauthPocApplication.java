07b5b8776f27c28ae81e633aa6af5ba1d1d1cf51
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-02 13:57:54 -0500

    init commit

---
+++ src/main/java/com/luis/spring/security/msauth_poc/MsauthPocApplication.java
@@ -0,0 +1,13 @@
+package com.luis.spring.security.msauth_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsauthPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsauthPocApplication.class, args);
+	}
+
+}
