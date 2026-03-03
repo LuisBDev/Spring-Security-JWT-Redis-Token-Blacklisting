b3f04c4c070c9a311d94ff34776f8257375035e8
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-03 11:49:07 -0500

    feat(auth): add setter for JWT properties to support configuration

--- src/main/java/com/luis/spring/security/msauth_poc/config/JwtProperties.java
+++ src/main/java/com/luis/spring/security/msauth_poc/config/JwtProperties.java
@@ -1,6 +1,7 @@
 package com.luis.spring.security.msauth_poc.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
  * @author Luis Balarezo
  **/
 @Getter
+@Setter //for spring to set the properties from application.yml
 @Component
 @ConfigurationProperties(prefix = "security.jwt")
 public class JwtProperties {
