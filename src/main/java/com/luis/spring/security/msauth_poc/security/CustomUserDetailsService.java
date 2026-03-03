a7065d7a49794e6991f4d43e3f80c7b4a9975dd8
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-03 10:16:45 -0500

    feat(auth): implement CustomUserDetailsService for user details retrieval

---
+++ src/main/java/com/luis/spring/security/msauth_poc/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.luis.spring.security.msauth_poc.security;
+
+import com.luis.spring.security.msauth_poc.entity.User;
+import com.luis.spring.security.msauth_poc.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Luis Balarezo
+ **/
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found with email: " + email));
+
+        return new CustomUserDetails(user);
+    }
+}
