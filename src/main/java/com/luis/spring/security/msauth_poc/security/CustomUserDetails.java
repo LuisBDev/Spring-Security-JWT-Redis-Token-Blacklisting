f72396f30fdb558d68724d432fe32319ba4c134a
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-03-03 10:16:39 -0500

    feat(auth): implement CustomUserDetails for user authentication

---
+++ src/main/java/com/luis/spring/security/msauth_poc/security/CustomUserDetails.java
@@ -0,0 +1,79 @@
+package com.luis.spring.security.msauth_poc.security;
+
+import com.luis.spring.security.msauth_poc.entity.Role;
+import com.luis.spring.security.msauth_poc.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * @author Luis Balarezo
+ **/
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        Set<Role> roles = user.getRoles();
+
+        roles.forEach(role -> {
+
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+
+            role.getPermissions()
+                    .forEach(
+                            permission -> authorities.add(new SimpleGrantedAuthority(permission.getName()))
+                    );
+
+        });
+
+        return authorities;
+    }
+
+    public UUID getId() {
+        return user.getId();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
